cce8ae5933cd4ed45a44c7a8d4b8ec9677cfac37
paulo-leitao <paulovictor.engca@gmail.com>
2022-05-04 13:38:50 -0300

    Criação do metodo Post para cadastro de novos correntistas.

--- src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -1,11 +1,11 @@
 package com.dio.santander.bankline.api.controller;
 
+import com.dio.santander.bankline.api.dto.NovoCorrentista;
 import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import com.dio.santander.bankline.api.service.CorrentistaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,9 +15,16 @@ public class CorrentistaController {
     @Autowired
     private CorrentistaRepository repository;
 
+    @Autowired
+    private CorrentistaService service;
+
     @GetMapping
     public List<Correntista> findAll(){
         return repository.findAll();
+    }
+    @PostMapping
+    public void save(@RequestBody NovoCorrentista correntista){
+        service.save(correntista);
     }
 
 }
