1fb70cbfaf7808cb12db01d7c3227ee9bda1964c
paulo-leitao <paulovictor.engca@gmail.com>
2022-05-04 01:34:48 -0300

    Criação do Pacote repositores com as interfaces e Criação do nosso controller Correntista

---
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -0,0 +1,23 @@
+package com.dio.santander.bankline.api.controller;
+
+import com.dio.santander.bankline.api.model.Correntista;
+import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/correntista")
+public class CorrentistaController {
+    @Autowired
+    private CorrentistaRepository repository;
+
+    @GetMapping
+    public List<Correntista> findAll(){
+        return repository.findAll();
+    }
+
+}
